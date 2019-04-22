482ed53a76954242bc5da480296daac48871098e
jiashaoyou <1170802374@qq.com>
2019-04-22 20:09:28 +0800

    commit

--- src/main/java/com/you/MySpringBootApplication.java
+++ src/main/java/com/you/MySpringBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringBootApplication {
       /**
-        * @Title: main  
+        * @Title:   
         * @Description: springboot项目的启动类
         * @param @param args  参数  
         * @return void    返回类型  

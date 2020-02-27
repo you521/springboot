b0e727696f879c861faa9e5cebf0901e0f569fb3
jiashaoyou <1170802374@qq.com>
2020-02-27 15:52:50 +0800

    添加文字说明

--- src/main/java/com/you/MySpringBootApplication.java
+++ src/main/java/com/you/MySpringBootApplication.java
@@ -3,6 +3,15 @@ package com.you;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * 
+    * @ClassName: MySpringBootApplication  
+    * @Description: 程序主入口
+    * @author you  
+    * @date 2020年2月27日  
+    *
+ */
+
 @SpringBootApplication
 public class MySpringBootApplication {
       /**
